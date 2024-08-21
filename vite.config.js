50bca4be125b8e8a304b64a21b6dd52840f3d1e7
phamgiagia <phamgiagia@gmail.com>
2024-08-21 08:06:00 +0000

    add auto detect html

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,34 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import * as glob from 'glob'
+
+// Function to automatically generate input entries
+function generateInputEntries() {
+    const pattern = './**/index.html';
+    const files = glob.sync(pattern, { absolute: true });
+
+    const entries = {};
+    files.forEach(file => {
+        const normalizedPath = file.replace(/\\/g, '/'); // Normalize path for Windows compatibility
+
+        // Skip files in any '/dist/' directory
+        if (!normalizedPath.includes('/dist/')) {
+            const pathSegments = normalizedPath.split('/');
+            // Create a key for the entries object using directory names just above 'index.html'
+            const name = pathSegments.slice(-3, -1).join('-'); // Customize this slicing as needed
+
+            // Ensure to resolve the file path correctly
+            entries[name] = resolve(file);
+        }
+    });
+
+    return entries;
+}
 
 export default defineConfig({
     build: {
         rollupOptions: {
-            input: {
-                'main': resolve(__dirname, '/index.html'),
-                'home': resolve(__dirname, 'home/index.html'),
-                'about': resolve(__dirname, 'about/index.html'),
-                'contact': resolve(__dirname, 'contact/index.html'),
-                'home-desktop': resolve(__dirname, '/home/desktop/index.html'),
-                'home-mobile': resolve(__dirname, '/home/mobile/index.html'),
-                'home-tablet': resolve(__dirname, '/home/tablet/index.html'),
-                'about-desktop': resolve(__dirname, '/about/desktop/index.html'),
-                'about-mobile': resolve(__dirname, '/about/mobile/index.html'),
-                'about-tablet': resolve(__dirname, '/about/tablet/index.html'),
-                'contact-desktop': resolve(__dirname, '/contact/desktop/index.html'),
-                'contact-mobile': resolve(__dirname, '/contact/mobile/index.html'),
-                'contact-tablet': resolve(__dirname, '/contact/tablet/index.html'),
-            },
+            input: generateInputEntries(),
         },
     },
 })
