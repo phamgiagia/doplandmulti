5793e0b48cb03cf54ab220cc5c67b4bac7496fef
phamgiagia <phamgiagia@gmail.com>
2024-08-23 18:07:47 +0000

    update without owner and pj name

--- html-transform-plugin.js
+++ html-transform-plugin.js
@@ -5,19 +5,15 @@ const HtmlTransformPlugin = createUnplugin(() => ({
     name: 'html-transform',
     enforce: 'pre',
     transformInclude(id) {
-        // This method can help identify HTML files or specific cases to apply transforms.
-        // Adjust the condition to fit your needs, for example, to target only HTML files.
         return id.endsWith('.html');
     },
     transform(code) {
-        // Using a regex to directly replace all matching src attributes in the HTML string
-        const updatedCode = code.replace(/src="https:\/\/raw\.githubusercontent\.com\/phamgiagia\/doplandmulti\/main\/[^"]+"/g, (match) => {
-            // Replace the full URL leaving only the path after the specific domain
-            return match.replace(/https:\/\/raw\.githubusercontent\.com\/phamgiagia\/doplandmulti\/main\//, '../../');
+        const updatedCode = code.replace(/src="https:\/\/raw\.githubusercontent\.com\/[^/]+\/[^/]+\/[^/]+\/[^"]+"/g, (match) => {
+            return match.replace(/https:\/\/raw\.githubusercontent\.com\/[^/]+\/[^/]+\/[^/]+\//, '../../');
         });
         return {
             code: updatedCode,
-            map: null, // Not needed unless you are dealing with source maps
+            map: null
         };
     }
 }));
